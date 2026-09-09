bb64d70a429f5e65b224089a6d51bc8140069b86
wikoci <git@dnax.io>
2026-09-09 11:36:15 +0000

    .

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,14 +3,8 @@ import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  ssr: true,
+  ssr: false,
   css: ['@/assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', "@dnax/ui", "@nuxtjs/tailwindcss"],
-
-  vite: {
-    server: {
-      allowedHosts:['ui.dnax.io']
-    }
-  }
 })
